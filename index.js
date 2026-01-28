d6389bc1c4c15b82c589ab6f96d8644f21a0e492
mudabeer <mohammadmudabeer20@gmail.com>
2026-01-28 18:45:44 +0530

    Refactor temperature conversion logic in index.js

--- index.js
+++ index.js
@@ -5,14 +5,7 @@ const cTof = document.getElementById("celsius-fahrenheit");
 const fToc = document.getElementById("fahrenheit-celsius");
 let temp;
 
-console.log("link sucessful")
-
 button.onclick = function(){
-    console.log("click working")
-    if(Boolean(inputvalue.value) != true){
-        result.textContent = "Empty input"
-    }
-    else{
+
         cTof.checked ? result.textContent = Number(inputvalue.value)*9/5+32 + "F" : fToc.checked ? result.textContent = (Number(inputvalue.value)-32)*(5/9) + "C": result.textContent = "Select the conversion type" ;
-    }
 }
